07c698c962dc589548cdf424689debac2f0ed45b
Will Shu <will.shu@bluecore.com>
2021-04-08 14:16:54 -0500

    remove spaces

--- pre_commit_hooks/check_tabs.py
+++ pre_commit_hooks/check_tabs.py
@@ -9,8 +9,9 @@ def replace_tabs_with_spaces(line, num_spaces=4):
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to check.')
+    parser.add_argument('fix-tabs', nargs='1', type=bool, help='Replaced tabs with spaces')
+    parser.add_argument('spaces', nargs='1', type=int, help='Number of spaces to replace tabs')
     args = parser.parse_args(argv)
-    retval = 0
     for filename in args.filenames:
         with open(filename, mode="r") as file:
             line_num = int(0)
@@ -18,15 +19,18 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
             list_of_lines = file.readlines()
             for num, line in enumerate(list_of_lines):
                 if '\t' in line:
-                    error = f"Tab found in line {line_num} \n"
+                    error = f"Tab found in {filename} on line {line_num} \n"
                     errors += error
-                    list_of_lines[num] = replace_tabs_with_spaces(line)
+                    if args.fix_tabs == True:
+                        with open(filename, mode="w") as file:
+                            list_of_lines[num] = replace_tabs_with_spaces(line, num_spaces=args.spaces)
                 line_num += 1
             if errors:
                 print(errors)
                 return 1
             else:
-                return retval
+                return 0
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
